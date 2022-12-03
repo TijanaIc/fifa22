2d6f676bcaee64e49044537598e16b8001992a2b
Tijana.Cvetkovic <KD481EQ@RS3358966W1>
2022-12-03 17:14:48 +0100

    read groups

--- Fifa22/Fifa22/DatabaseHelper.cs
+++ Fifa22/Fifa22/DatabaseHelper.cs
@@ -10,8 +10,10 @@ namespace Fifa22
 {
     public class DatabaseHelper
     {
-        public static DataTable ExecuteQuery(string connectionString, string query)
+        static string connectionString = "Server=localhost;Database=Fifa22;Trusted_Connection=True;";
+        public static DataTable ExecuteQuery(string query)
         {
+
             DataTable dataTable = new DataTable();
 
             SqlConnection conn = new SqlConnection(connectionString);
@@ -21,10 +23,18 @@ namespace Fifa22
             // create data adapter
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             // this will query your database and return the result to your datatable
-            da.Fill(dataTable);
+            try
+            {
+                da.Fill(dataTable);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error!");
+            }
             conn.Close();
             da.Dispose();
             return dataTable;
         }
+        
     }
 }
--- Fifa22/Fifa22/Program.cs
+++ Fifa22/Fifa22/Program.cs
@@ -1,15 +1,35 @@
 ﻿using Fifa22;
 using System.Data;
 
-DataTable table = DatabaseHelper.ExecuteQuery("Server=localhost;Database=Fifa22;Trusted_Connection=True;", "select * from Team");
+//string[] groups1 = { "A", "B", "C", "Z" };
+//string[] groups2 = { "X", "Y", "Z", "A", "B", "K" };
 
-foreach (DataRow row in table.Rows)
+DataTable tableGroups = DatabaseHelper.ExecuteQuery("select distinct Team_group from Team");
+foreach (DataRow row in tableGroups.Rows)
 {
-    Team t = new Team();
-    t.Team_id = Convert.ToInt32(row["Team_id"]);
-    Console.WriteLine(t.Team_id);
-    t.Team_name = row["Team_name"].ToString();
-    Console.WriteLine(t.Team_name);
-    t.Team_group = row["Team_group"].ToString();
-    Console.WriteLine(t.Team_group); 
+    string group = row["Team_group"].ToString();
+    Console.WriteLine(group);
+    DataTable teams = DatabaseHelper.ExecuteQuery($"select Team_name from Team where Team_group='{group}'");
+    foreach (DataRow teamRow in teams.Rows)
+    {
+        Console.WriteLine(teamRow["Team_name"].ToString());
+    }
 }
+
+//string[] groups = { "A", "B", "C" };
+//foreach (string group in groups)
+//{
+//    Console.WriteLine(group);
+//    DataTable table = DatabaseHelper.ExecuteQuery($"select Team_name from Team where Team_group='{group}'");
+//    foreach (DataRow row in table.Rows)
+//    {
+//        //Team t = new Team();
+//        //t.Team_id = Convert.ToInt32(row["Team_id"]);
+//        //Console.WriteLine(t.Team_id);
+//        //t.Team_name = row["Team_name"].ToString();
+//        Console.WriteLine(row["Team_name"].ToString());
+//        //t.Team_group = row["Team_group"].ToString();
+//        //Console.WriteLine(t.Team_group);
+//    }
+//}
+
