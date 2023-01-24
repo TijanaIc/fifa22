fd4a01b1b274eca4554e497df6d79028bba2ee91
Tijana Cvetkovic <KD481EQ@RS3358966W1>
2023-01-24 20:06:57 +0100

    clean up

--- Fifa22/Fifa22/Program.cs
+++ Fifa22/Fifa22/Program.cs
@@ -2,33 +2,6 @@
 using Fifa22.Library;
 
 
-//IDataReader datareader = new ServiceReader();
-//Console.WriteLine("Press any key to start application");
-//Console.ReadLine();
-
-//List<Group> groups = datareader.GetGroups();
-//foreach (var group in groups)
-//{
-//    Console.WriteLine(group.Name);
-//    List<Team> teams = datareader.GetTeamByName(group.Name);
-//    foreach(var team in teams)
-//    {
-//        Console.WriteLine(team.Team_name);
-//        List<Player> players = datareader.GetPlayersFromTeam(team.Team_id);
-//        foreach (var player in players)
-//        {
-//            Console.WriteLine(player.FirstName + " " + player.LastName);
-//        }
-//    }
-//}
-
-//List<TeamEx> teamsg = datareader.GetTeamByGoal(5);
-//foreach(var team in teamsg)
-//{
-//    Console.WriteLine(team.Team_name);
-//}
-
-
 PrintMessage d = new PrintMessage(PrintMessageToConsole);
 d("Tijana");
 Console.ReadLine();
